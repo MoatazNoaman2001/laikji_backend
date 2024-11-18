848b0a88bb1beb9a0f6c172abbc96fd1caafe67d
Ameera Ghariba <sweet-meero@hotmail.com>
2024-11-18 13:43:30 +0300

    updateMic

--- routes/roomAdmin/roomRoutes.js
+++ routes/roomAdmin/roomRoutes.js
@@ -482,16 +482,18 @@ router.post('/update', img_uploader.single('welcome_img'), async (req, res) => {
             helpers.resizeImage('rooms/' + req.file.filename, true, 900);
         }
         console.log('update request ' + JSON.stringify(req.room, null, 2));
+        console.log('update request2 ' + JSON.stringify(req.body, null, 2));
+        console.log('update request3 ' + JSON.stringify(req.room.mic, null, 2));
 
         var update = {
             title: req.body.title ?? room.title,
             description: req.body.description ?? room.description,
             lock_msg: req.body.lock_msg ?? room.lock_msg,
             mic: {
-                mic_permission: req.room.mic_permission,
-                talk_dur: req.room.talk_dur,
-                mic_setting: req.room.mic_setting,
-                shared_mic_capacity: req.room.shared_mic_capacity,
+                mic_permission: req.room.mic.mic_permission,
+                talk_dur: req.room.mic.talk_dur,
+                mic_setting: req.room.mic.mic_setting,
+                shared_mic_capacity: req.room.mic.shared_mic_capacity,
             },
             private_status:
                 req.body.private_status && req.body.private_status in ['0', '1', '2', '3']
