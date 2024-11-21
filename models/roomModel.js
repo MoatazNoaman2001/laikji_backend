50e1bc0f394d70057225d03b3320be6f74bb59c7
MoatazNoaman <moataz.noaman12@gmail.com>
2024-11-21 12:47:59 +0200

    edit mic model

--- models/roomModel.js
+++ models/roomModel.js
@@ -47,7 +47,7 @@ const schema = Schema({
         },
     },
     mic: {
-        mic_permission: { type: Number },
+        mic_permission: { type: Number , default: 0},
         talk_dur: { type: [Number], default: [300, 300, 300, 300, 300] },
         mic_setting: { type: [Boolean], default: [false, true, true, false] },
         shared_mic_capacity: { type: Number, default: 3 },
