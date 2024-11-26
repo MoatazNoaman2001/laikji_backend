aabeb089f794dfeed32c5ff0c9a0f97fb82241ff
Ameera Ghariba <sweet-meero@hotmail.com>
2024-11-26 12:11:47 +0300

    0permission

--- socketHandlers/roomSocketHandler.js
+++ socketHandlers/roomSocketHandler.js
@@ -1118,6 +1118,7 @@ module.exports = (io) => {
                     console.log('permission is ' + newRoom.mic.mic_permission);
                     if (user) {
                         if (
+                            newRoom.mic.mic_permission !== 0 ||
                             newRoom.mic.mic_permission === 1 ||
                             (newRoom.mic.mic_permission === 2 &&
                                 user.type !== enums.userTypes.guest) ||
