36e0eaf8595ea79be93f9c16bb0dfc1ca35fc884
Ameera Ghariba <sweet-meero@hotmail.com>
2024-12-05 17:59:29 +0300

    mic-queue

--- socketHandlers/roomSocketHandler.js
+++ socketHandlers/roomSocketHandler.js
@@ -1763,16 +1763,7 @@ module.exports = (io) => {
                     }
 
                     const nextUserId = micQueue[xroomId].shift(); // Get the next user from the queue
-                    io.to(xroomId).emit('mic-queue-update', micQueue[xroomId]);
-
-                    console.log(
-                        `Assigning mic to user: ${nextUserId}. Queue length: ${
-                            Array.from(micQueue[xroomId]).length
-                        }`,
-                    );
-
                     const nextUser = await getUserById(nextUserId, xroomId);
-                    console.log('status is' + nextUser.status);
                     if (
                         !nextUser ||
                         roomInfo.speakers.has(nextUserId) ||
@@ -1782,9 +1773,18 @@ module.exports = (io) => {
                             ` User ${nextUserId} is already a speaker or not found. Skipping...`,
                         );
                         micAssigning = false;
+                        micQueue.splice(0, 0, nextUserId);
                         await assignMic(); // Recursively try the next user
                         return;
                     }
+
+                    console.log(
+                        `Assigning mic to user: ${nextUserId}. Queue length: ${
+                            Array.from(micQueue[xroomId]).length
+                        }`,
+                    );
+
+                    io.to(xroomId).emit('mic-queue-update', micQueue[xroomId]);
 
                     const room = await roomModel.findById(xroomId);
                     if (!room) return;
