00a69b997608e87acc47bad37f442359a2e09c1d
Ameera Ghariba <sweet-meero@hotmail.com>
2024-12-14 11:50:20 +0300

    edit3

--- helpers/micHelpers.js
+++ helpers/micHelpers.js
@@ -137,8 +137,8 @@ const assignMic = async (xroomId, roomInfo) => {
         micAssigning = true; // Lock mic assignment immediately
         try {
             while (roomInfo.micQueue.length > 0) {
-                let nextUserId = roomInfo.micQueue[0];
-                //global.io.to(xroomId).emit('mic-queue-update', roomInfo.micQueue);
+                let nextUserId = roomInfo.micQueue.shift();
+                global.io.to(xroomId).emit('mic-queue-update', roomInfo.micQueue);
 
                 console.log(
                     `Attempting to assign mic to user: ${nextUserId}. Queue length: ${roomInfo.micQueue.length}`,
@@ -174,7 +174,7 @@ const assignMic = async (xroomId, roomInfo) => {
                         console.log('Room not found. Exiting mic assignment.');
                         break;
                     }
-                    roomInfo.micQueue = roomInfo.micQueue.shift();
+                    // roomInfo.micQueue = roomInfo.micQueue.shift();
                     global.io.to(xroomId).emit('mic-queue-update', roomInfo.micQueue);
 
                     await assignSpeaker(roomInfo, nextUserId, nextUser, room, xroomId);
