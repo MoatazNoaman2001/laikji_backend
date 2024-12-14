3b270436c6aa8fdb0c6899d7283009ddb438e9e5
Ameera Ghariba <sweet-meero@hotmail.com>
2024-12-14 11:03:27 +0300

    queue

--- helpers/micHelpers.js
+++ helpers/micHelpers.js
@@ -170,11 +170,10 @@ const assignMic = async (xroomId, roomInfo) => {
                     );
 
                     // Place nextUserId at index 1 of the queue
-                    if (roomInfo.micQueue.length >= 1) {
+                    if (roomInfo.micQueue.length > 0) {
                         roomInfo.micQueue.splice(1, 0, nextUserId); // Insert at index 1
+                        global.io.to(xroomId).emit('mic-queue-update', roomInfo.micQueue);
                     }
-
-                    global.io.to(xroomId).emit('mic-queue-update', roomInfo.micQueue);
 
                     micAssigning = false;
                     processedUsers.add(nextUserId);
