3344c8ba88129e62e8f8101cb0f2f4947428290a
Ameera Ghariba <sweet-meero@hotmail.com>
2024-12-19 08:11:20 +0300

    re

--- socketHandlers/roomSocketHandler.js
+++ socketHandlers/roomSocketHandler.js
@@ -622,7 +622,6 @@ module.exports = (io) => {
                     pc.user1Ref._id.toString() == xuser._id.toString() ? pc.user2Ref : pc.user1Ref;
 
                 otherUser = await getUserById(otherUser._id, xroomId);
-                const room = await roomModel.findById(xroomId);
 
                 if (!otherUser.can_private_chat || !otherUser.server_can_private_chat) {
                     io.to(xuser.socketId).emit('new-alert', {
@@ -653,45 +652,6 @@ module.exports = (io) => {
                     }
                 }
 
-                if (room.private_status == 3) {
-                    console.log('user has deleted his messages');
-                    if (
-                        (otherUser._id == pc.user1Ref._id.toString() && pc.isUser1Deleted) ||
-                        (otherUser._id == pc.user2Ref._id.toString() &&
-                            pc.isUser2Deleted &&
-                            ![
-                                enums.userTypes.mastermain.toString(),
-                                enums.userTypes.chatmanager.toString(),
-                                enums.userTypes.root.toString(),
-                                enums.userTypes.master.toString(),
-                                enums.userTypes.mastergirl.toString(),
-                            ].includes(xuser.type.toString()))
-                    ) {
-                        io.to(xuser.socketId).emit('new-alert', {
-                            ok: false,
-                            msg_en: 'Private chat is available for admins only',
-                            msg_ar: 'الرسائل الخاصة في هذه الغرفة متاحة للمشرفين فقط',
-                        });
-                        return;
-                    }
-                }
-                if (room.private_status == 2) {
-                    console.log('user has deleted his messages');
-                    if (
-                        (otherUser._id == pc.user1Ref._id.toString() && pc.isUser1Deleted) ||
-                        (otherUser._id == pc.user2Ref._id.toString() &&
-                            pc.isUser2Deleted &&
-                            xuser.type.toString() === enums.userTypes.guest.toString())
-                    ) {
-                        io.to(xuser.socketId).emit('new-alert', {
-                            ok: false,
-                            msg_en: 'Private chat is available for admins only',
-                            msg_ar: 'الرسائل الخاصة في هذه الغرفة متاحة للمشرفين والأعضاء فقط',
-                        });
-                        return;
-                    }
-                }
-
                 pc.isUser1Deleted = false;
                 pc.isUser2Deleted = false;
                 pc.save();
@@ -705,7 +665,6 @@ module.exports = (io) => {
                 };
 
                 if (data.replay) {
-                    console.log('data is replay');
                     body.replay = {
                         type: data.replay.type,
                         msg: data.replay.msg,
@@ -738,8 +697,9 @@ module.exports = (io) => {
                     isRead: false,
                 });
 
+                const room = await roomModel.findById(xroomId);
                 const otherRoom = await roomModel.findById(
-                    room.isMeeting ? room.meetingRef : room.parentRef,
+                    room.isMeeting ? room.parentRef : room.meetingRef,
                 );
 
                 let otherUserInOtherRoom = null;
@@ -758,11 +718,8 @@ module.exports = (io) => {
                     },
                     msg: msg,
                 });
-                console.log('private message .1');
 
                 if (otherUserInOtherRoom) {
-                    console.log('private message .2');
-
                     io.to(otherUserInOtherRoom.socketId).emit('new-private-msg', {
                         chat: {
                             ...pc,
@@ -781,11 +738,8 @@ module.exports = (io) => {
                     },
                     msg: msg,
                 });
-                console.log('private message .3');
 
                 if (xuserInOtherRoom) {
-                    console.log('private message .4');
-
                     io.to(xuserInOtherRoom.socketId).emit('new-private-msg', {
                         chat: {
                             ...pc,
