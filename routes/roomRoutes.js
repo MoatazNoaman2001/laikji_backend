3dd1758d4647d879b35498325a1100625c4da169
Ameera Ghariba <sweet-meero@hotmail.com>
2024-12-31 09:19:57 +0300

    defineObject

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -8,6 +8,7 @@ const { getSettings, hexToXRgb } = require('../helpers/tools');
 const router = express.Router();
 const mediasoup = require('mediasoup');
 const memberModal = require('../models/memberModal');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 router.get('/all', async (req, res) => {
     var response = [];
