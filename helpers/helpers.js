6e92ad773e0d7b05906b9ba876c0b760b9c9cb5c
Ameera Ghariba <sweet-meero@hotmail.com>
2025-01-06 07:28:25 +0300

    undo

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -22,9 +22,6 @@ const roomModel = require('../models/roomModel');
 const groupModel = require('../models/groupModel');
 const reportModel = require('../models/reportModel');
 
-const fs = require('fs');
-const path = require('path');
-
 function generateKey(length = 32) {
     return crypto
         .randomBytes(Math.ceil(length / 2))
@@ -295,19 +292,15 @@ function resizeImage(path, inPublic = true, width = 150) {
 }
 
 async function saveMulterFile(file, folder, name = null) {
-    const dest_folder = `public/${folder}`;
     const dest_file =
-        `${dest_folder}/` +
+        'public/' +
+        folder +
+        '/' +
         (name ? name : generateKey(8) + '-' + Date.now()) +
         path.extname(file.originalname);
 
-    // Ensure the destination folder exists
-    await fs.promises.mkdir(dest_folder, { recursive: true });
+    await fs.writeFileSync(dest_file, file.buffer); //
 
-    // Write the file to the destination
-    await fs.promises.writeFile(dest_file, file.buffer);
-
-    // Return the relative path
     return dest_file.replace('public/', '');
 }
 
