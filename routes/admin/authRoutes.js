178935868de59e82ea5a14b32a2ea0580a7e79b8
Ameera Ghariba <sweet-meero@hotmail.com>
2025-01-09 05:20:08 +0300

    fff

--- routes/admin/authRoutes.js
+++ routes/admin/authRoutes.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const User = require('../../models/registeredUserModal');
 const router = express.Router();
+const helpers = require('../../helpers/helpers');
 // const {
 //     generateVerificationToken,
 //     sendVerificationEmail,
