329aa88e70e5ec40d9936f2f7a78b346bbc93141
Ameera Ghariba <sweet-meero@hotmail.com>
2025-01-12 06:42:03 +0300

    add

--- routes/roomAdmin/roomRoutes.js
+++ routes/roomAdmin/roomRoutes.js
@@ -81,7 +81,6 @@ router.get('/info', async (req, res) => {
     try {
         let room = req.room;
         response = await getRoomInfo(room);
-        console.log('response is ' + JSON.stringify(response, null, 2));
         return res.status(200).send({
             ok: true,
             data: response,
