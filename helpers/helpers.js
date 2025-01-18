b9cb4f2fd9ad9c936ee2ceca17c14c97015fd99c
Ameera Ghariba <sweet-meero@hotmail.com>
2025-01-18 09:00:22 +0300

    jjf

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -138,40 +138,41 @@ async function public_room(room) {
         var chats = await chatModel.find({
             roomRef: room._id,
         });
+        if (room_id != '673a4395c92256f6a78c3c14') {
+            console.log('room name: ', room.name);
+            var mainChat = chats.find((c) => c.isMain == true)._id;
 
-        console.log('room name: ', room.name);
-        var mainChat = chats.find((c) => c.isMain == true)._id;
-
-        return {
-            _id: room._id,
-            serial: room.serial,
-            parentRef: room.parentRef,
-            meetingRef: room.meetingRef,
-            name: room.name,
-            title: room.title,
-            ad_text: settings.ad_text,
-            description: room.description,
-            icon: simg(room.icon),
-            mainChat: mainChat,
-            chats: chats,
-            isGold: room.isGold,
-            isSpecial: room.isSpecial,
-            isMeeting: room.isMeeting,
-            type: room.isGold ? 'ذهبية' : room.isSpecial ? 'مميزة' : 'عادية',
-            outside_style: room.outside_style,
-            inside_style: room.inside_style,
-            private_status: room.private_status,
-            lock_status: room.lock_status,
-            lock_msg: room.lock_msg,
-            welcome: {
-                img: room.welcome.img ? simg(room.welcome.img) : '',
-                text: room.welcome.text,
-                direction: room.welcome.direction,
-                color: room.welcome.color,
-            },
-            mic: room.mic,
-            colors: await getAppUsersColors(),
-        };
+            return {
+                _id: room._id,
+                serial: room.serial,
+                parentRef: room.parentRef,
+                meetingRef: room.meetingRef,
+                name: room.name,
+                title: room.title,
+                ad_text: settings.ad_text,
+                description: room.description,
+                icon: simg(room.icon),
+                mainChat: mainChat,
+                chats: chats,
+                isGold: room.isGold,
+                isSpecial: room.isSpecial,
+                isMeeting: room.isMeeting,
+                type: room.isGold ? 'ذهبية' : room.isSpecial ? 'مميزة' : 'عادية',
+                outside_style: room.outside_style,
+                inside_style: room.inside_style,
+                private_status: room.private_status,
+                lock_status: room.lock_status,
+                lock_msg: room.lock_msg,
+                welcome: {
+                    img: room.welcome.img ? simg(room.welcome.img) : '',
+                    text: room.welcome.text,
+                    direction: room.welcome.direction,
+                    color: room.welcome.color,
+                },
+                mic: room.mic,
+                colors: await getAppUsersColors(),
+            };
+        }
     } catch (err) {
         console.log('error from public room ', err.toString());
     }
