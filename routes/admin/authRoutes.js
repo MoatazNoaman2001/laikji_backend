8b6952225a305d6235fc5c5f1da4fdffe39a6394
Ameera Ghariba <sweet-meero@hotmail.com>
2025-01-22 14:01:23 +0300

    id

--- routes/admin/authRoutes.js
+++ routes/admin/authRoutes.js
@@ -144,7 +144,7 @@ router.get('/:id', async (req, res) => {
     const id = req.params.id;
     try {
         let item = await User.find({
-            _id: new ObjectId(id),
+            _id: id,
         });
 
         res.status(200).send({
