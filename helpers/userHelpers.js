0a8cf906bb8d6b0eb01ff3343376fa704feb7994
Ameera Ghariba <sweet-meero@hotmail.com>
2025-01-29 21:40:17 +0300

    userDevice

--- helpers/userHelpers.js
+++ helpers/userHelpers.js
@@ -226,7 +226,7 @@ const getUserById = async (user_id, room_id) => {
         ...regUser,
         regUserRef: regUser && regUser._id ? regUser._id : null,
         ...user._doc,
-        device,
+        device: user.device,
         server_stop_remaining,
         is_spy,
         is_visible: !is_hidden,
