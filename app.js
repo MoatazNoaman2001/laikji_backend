a830fd79958f65e8eb77d84033a1f00b738073b1
Ameera Ghariba <sweet-meero@hotmail.com>
2025-01-30 15:11:17 +0300

    kk

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const memberPrivateMiddleware = require('./middlewares/memberPrivateMiddleware')
 const { getNowDateTime } = require('./helpers/tools');
 const http = require('http');
 const authCheckMiddleware = require('./middlewares/authCheckMiddleware');
-
+const group = require('./routes/admin/groupsRoutes');
 //#region if ssl enabled
 // const https = require('https');
 // const fs = require('fs');
@@ -55,7 +55,7 @@ app.use('/:roomId', require('./routes/micRoutes'));
 app.use('/room_admin/:room_id', roomAdminMiddleware, require('./routes/roomAdmin/roomRoutes'));
 app.use('/room_cmd/:room_id', roomAdminMiddleware, require('./routes/roomAdmin/commandsRoutes'));
 // Admin Routes
-app.use('/admin/groups', require('./routes/admin/groupsRoutes'));
+app.use('/admin/groups', group);
 app.use('/admin/entericons', require('./routes/admin/enterIconsRoutes'));
 app.use('/admin/emojis', require('./routes/admin/emojisRoutes'));
 app.use('/admin/rooms', require('./routes/admin/roomsRoutes'));
@@ -66,7 +66,6 @@ app.use('/admin/reports', require('./routes/admin/reportsRoutes'));
 app.use('/admin/settings', require('./routes/admin/settingsRoutes'));
 app.use('/admin/global', require('./routes/admin/globalRoutes'));
 app.use('/admin/auth', require('./routes/admin/authRoutes'));
-app.use('/admin/groups/', authCheckMiddleware, require('./routes/admin/groupsRoutes'));
 
 // Member Routes
 app.use(
