2e7863482feba9b99e6124e82b30bfcc416a0614
Ameera Ghariba <sweet-meero@hotmail.com>
2025-01-30 20:55:51 +0300

    ff

--- routes/admin/authRoutes.js
+++ routes/admin/authRoutes.js
@@ -143,6 +143,13 @@ router.get('/:id', async (req, res) => {
 
 router.put('/:id', authCheckMiddleware, async (req, res) => {
     const id = req.params.id;
+    console.log(
+        'auth put',
+        req.body.username,
+        req.body.email,
+        req.body.password,
+        req.body.permissions,
+    );
     let update = {
         username: req.body.username,
         password: req.body.password,
