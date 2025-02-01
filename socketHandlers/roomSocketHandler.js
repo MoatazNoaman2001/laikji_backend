98f71201154e6639a0ffddb06b1031ae90486881
Ameera Ghariba <sweet-meero@hotmail.com>
2025-02-01 13:12:49 +0300

    jjf

--- socketHandlers/roomSocketHandler.js
+++ socketHandlers/roomSocketHandler.js
@@ -415,6 +415,7 @@ module.exports = (io) => {
         if (!xuser) {
             xuser = await createUser(key, xroomId, member, regUser_id);
         }
+        console.log('new user is ', JSON.stringify(xuser, null, 2));
 
         let os = xclient.handshake.query.os;
         if (
@@ -469,6 +470,7 @@ module.exports = (io) => {
             xuser._id,
             xroomId,
         );
+        console.log('update user is ', JSON.stringify(xuser, null, 2));
 
         /////////////// ROOM LOGIN SUCCESS CASE ///////////////////
         const roomInfo = await getRoomData(xroomId);
